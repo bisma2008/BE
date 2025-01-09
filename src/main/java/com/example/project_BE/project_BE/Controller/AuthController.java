7971e01ec8cf4dbf6bc85737f8127fd3d059ae31
bismaputra436@gmail.com <bismaputra436@gmail.com>
2025-01-09 14:25:13 +0700

    update

--- src/main/java/com/example/project_BE/project_BE/Controller/AuthController.java
+++ src/main/java/com/example/project_BE/project_BE/Controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/log")
 @CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
